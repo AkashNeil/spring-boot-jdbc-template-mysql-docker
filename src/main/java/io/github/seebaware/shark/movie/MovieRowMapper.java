58a0daffa13fe7544cd8929160126868ba1ff9c6
Neil <akashneil@live.com>
2021-09-19 17:19:29 +0400

    .

--- src/main/java/io/github/seebaware/shark/movie/MovieRowMapper.java
+++ src/main/java/io/github/seebaware/shark/movie/MovieRowMapper.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class MovieRowMapper implements RowMapper<Movie> {
 
@@ -15,7 +16,7 @@ public class MovieRowMapper implements RowMapper<Movie> {
         return new Movie(
                 resultSet.getInt("id"),
                 resultSet.getString("name"),
-                null,
+                List.of(),
                 resultSet.getString("releaseDate")
         );
 
